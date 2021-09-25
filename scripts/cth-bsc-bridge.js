7c6b046c61618870f128581327c9b83d1b65ea99
Tcheer <29361785+kiniwo@users.noreply.github.com>
2021-09-24 22:40:26 -0400

    Update cth-bsc-bridge.js

--- scripts/cth-bsc-bridge.js
+++ scripts/cth-bsc-bridge.js
@@ -1,9 +1,9 @@
 const Web3 = require('web3');
-const BridgeEth = require('../build/contracts/BridgeEth.json');
+const BridgeCth = require('../build/contracts/BridgeCth.json');
 const BridgeBsc = require('../build/contracts/BridgeBsc.json');
 
 const web3Eth = new Web3('Infura Rinkeby  url');
-const web3Bsc = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
+const web3Bsc = new Web3('https://bsc-dataseed1.binance.org:443');
 const adminPrivKey = '';
 const { address: admin } = web3Bsc.eth.accounts.wallet.add(adminPrivKey);
 
